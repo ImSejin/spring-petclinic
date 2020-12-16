86400a3904715838c5c98aca34cb7bbb5951ae6b
ImSejin <hackeres123@gmail.com>
2020-12-16 11:35:23 +0900

    ✨ Add: field `age` in `Person`

--- src/main/java/org/springframework/samples/petclinic/model/Person.java
+++ src/main/java/org/springframework/samples/petclinic/model/Person.java
@@ -18,6 +18,7 @@ package org.springframework.samples.petclinic.model;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
 
 /**
  * Simple JavaBean domain object representing an person.
@@ -35,6 +36,10 @@ public class Person extends BaseEntity {
 	@NotEmpty
 	private String lastName;
 
+	@Column(name = "age")
+	@Positive
+	private int age;
+
 	public String getFirstName() {
 		return this.firstName;
 	}
@@ -49,6 +54,14 @@ public class Person extends BaseEntity {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 }
